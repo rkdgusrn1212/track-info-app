6cc9ca9e3f86aa32294e8fd971cdc5f5f189c8d2
Kang Hyungu <khgkjg12@naver.com>
2018-06-27 08:11:33 +0900

    일단 화장실가느라 미리 드롭다운 커밋함. 이제 다녀와서 우선순위에 따라 자르는 작업을 하겠습니다.

--- front/src/components/Header/Header.js
+++ front/src/components/Header/Header.js
@@ -8,6 +8,7 @@
  */
 
 import React from 'react';
+import TrackInfo from '../../TrackInfo';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import {
   Nav,
@@ -15,6 +16,7 @@ import {
   NavDropdown,
   MenuItem,
   ProgressBar,
+  Alert
 } from 'react-bootstrap';
 import Navbar, {Brand} from 'react-bootstrap/lib/Navbar';
 import history from '../../core/history';
@@ -56,76 +58,111 @@ class Header extends React.Component{
     );
   }
 
-  render() {
-  return (
-    <div id="wrapper" className="content">
-      <Navbar fluid={true}>
-          <Brand>
-            <span>
-              <img src={logo} alt="Start React" title="Start React"/>
-              <span>&nbsp;<b>ACADEMIA</b></span>
-                <button type="button" className="navbar-toggle" onClick={() => {toggleMenu();}} style={{position: 'absolute', right: 0, top: 0}}>
-                  <span className="sr-only">Toggle navigation</span>
-                  <span className="icon-bar"></span>
-                  <span className="icon-bar"></span>
-                  <span className="icon-bar"></span>
-                </button>
-            </span>
-          </Brand>
-          <ul className="nav navbar-top-links navbar-right">
-           <NavDropdown title={<span><i className="fa fa-tasks fa-fw"></i> </span>} id = 'navDropdown2222'>
-                  <MenuItem eventKey="1" style={ {width: 300} }>
-                    <div>
-                      <p> <strong>Task 1</strong> <span className="pull-right text-muted">40% Complete</span> </p>
-                      <div>
-                        <ProgressBar  bsStyle="success" now={40} />
-                      </div>
-                    </div>
-                  </MenuItem>
-                  <MenuItem divider />
-                  <MenuItem eventKey="2">
-                    <div>
-                      <p> <strong>Task 2</strong> <span className="pull-right text-muted">20% Complete</span> </p>
-                      <div>
-                        <ProgressBar  bsStyle="info" now={20} />
-                      </div>
-                    </div>
-                  </MenuItem>
-                  <MenuItem divider />
-                  <MenuItem eventKey="3">
-                    <div>
-                      <p> <strong>Task 3</strong> <span className="pull-right text-muted">60% Complete</span> </p>
-                      <div>
-                        <ProgressBar  bsStyle="warning" now={60} />
-                      </div>
-                    </div>
-                  </MenuItem>
-                  <MenuItem divider />
-                  <MenuItem eventKey="4">
-                    <div>
-                      <p> <strong>Task 4</strong> <span className="pull-right text-muted">80% Complete</span> </p>
-                      <div>
-                        <ProgressBar  bsStyle="danger" now={80} />
-                      </div>
-                    </div>
-                  </MenuItem>
-                  <MenuItem divider />
-                  <MenuItem eventKey="5">
-                      <strong>See All Tasks</strong> <i className="fa fa-angle-right"></i>
-                  </MenuItem>
-                </NavDropdown>
-
-
-                  {localStorage.getItem('user')!=null?this.renderLogout():this.renderLogin()}
-
-          </ul>
-          <div className="header-decoration">
-            <img className="bar-img" src={sejongBar}/>
-            <img className="bar-title" src={sejongBarTitle}/>
+  renderSingleTrack(trackName, trackSpec, lectures){
+    let pCount = 0;//필수 충족수
+    let pTotal = trackSpec["트랙기초교과"].length;//전체 필수과목
+    let cCount = 0;//선택 충족수
+    let cTotal;
+    let pText="";
+    let cText="";
+    if(trackSpec["트랙응용교과"].length>6){
+      cTotal = 6;
+    }else{
+      cTotal = trackSpec["트랙응용교과"].length;
+    }
+    for(let i=0; i< trackSpec["트랙기초교과"].length; i++){
+      for(let j in lectures){
+        if(lectures[j][7]==trackSpec["트랙기초교과"][i]){
+          pCount++;
+          if(pCount>1){
+            pText+=", ";
+          }
+          pText+=trackSpec["트랙기초교과"][i];
+        }
+      }
+    }
+    for(let i=0; i< trackSpec["트랙응용교과"].length; i++){
+      for(let j in lectures){
+        if(lectures[j][7]==trackSpec["트랙응용교과"][i]){
+          cCount++;
+          if(cCount>1){
+            cText+=", ";
+          }
+          cText+=trackSpec["트랙응용교과"][i];
+        }
+      }
+    }
+    let percent = parseInt((pCount+cCount)*100/(pTotal+cTotal));
+    return(
+      <MenuItem style={ {width: 300} }>
+        <div>
+          <p> <strong>{trackName}</strong> <span className="pull-right text-muted">{percent}% Complete</span> </p>
+          <div>
+            <ProgressBar  bsStyle="danger" now={percent} />
           </div>
-      </Navbar>
-    </div>
-  );
+        </div>
+      </MenuItem>
+    );
+  }
+
+  renderDivider(){
+    return <MenuItem divider/>;
+  }
+
+  renderTrack(lectures){
+    if(lectures == null){
+      return(
+          <Alert bsStyle="warning">로그인이 먼저 필요합니다.</Alert>);
+    }
+    let items = [];
+    for(let key in TrackInfo){
+      items.push(this.renderSingleTrack(key,TrackInfo[key],lectures));
+      items.push(this.renderDivider());
+    }
+    return(
+
+       <NavDropdown title={<span><i className="fa fa-tasks fa-fw"></i> </span>} id = 'navDropdown2222'>
+        {items}
+        <MenuItem eventKey="5">
+            <strong>See All Tasks</strong> <i className="fa fa-angle-right"></i>
+        </MenuItem>
+  </NavDropdown>
+    );
+  }
+
+  render() {
+    let lectures;
+    if(localStorage.getItem('user')!=null){
+      lectures = JSON.parse(localStorage.getItem('user')).lecture;
+    }else{
+      lectures = null;
+    }
+    return (
+      <div id="wrapper" className="content">
+        <Navbar fluid={true}>
+            <Brand>
+              <span>
+                <img src={logo} alt="Start React" title="Start React"/>
+                <span>&nbsp;<b>ACADEMIA</b></span>
+                  <button type="button" className="navbar-toggle" onClick={() => {toggleMenu();}} style={{position: 'absolute', right: 0, top: 0}}>
+                    <span className="sr-only">Toggle navigation</span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                  </button>
+              </span>
+            </Brand>
+            <ul className="nav navbar-top-links navbar-right">
+                    {this.renderTrack(lectures)}
+                    {localStorage.getItem('user')!=null?this.renderLogout():this.renderLogin()}
+            </ul>
+            <div className="header-decoration">
+              <img className="bar-img" src={sejongBar}/>
+              <img className="bar-title" src={sejongBarTitle}/>
+            </div>
+        </Navbar>
+      </div>
+    );
   }
 }
 function toggleMenu(){
