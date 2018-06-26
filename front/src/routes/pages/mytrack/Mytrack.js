a677f6efad3e3007cf2cb2e81abb884f9e7e0324
Kang Hyungu <khgkjg12@naver.com>
2018-06-27 06:52:54 +0900

    비율 보여주기 기능 구현

--- front/src/routes/pages/mytrack/Mytrack.js
+++ front/src/routes/pages/mytrack/Mytrack.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
-import { Col, Row, PageHeader, ProgressBar, Panel } from 'react-bootstrap';
+import { Col, Row, PageHeader, ProgressBar, Panel, Alert } from 'react-bootstrap';
+import TrackInfo from '../../../TrackInfo';
 
 const title = "나의 트랙이수현황";
 
@@ -11,7 +12,80 @@ class Mytrack extends React.Component {
         context.setTitle(title);
     }
 
+    renderSingleTrack(trackName, trackSpec, lectures){
+      let pCount = 0;//필수 충족수
+      let pTotal = trackSpec["트랙기초교과"].length;//전체 필수과목
+      let cCount = 0;//선택 충족수
+      let cTotal;
+      let pText="";
+      let cText="";
+      if(trackSpec["트랙응용교과"].length>6){
+        cTotal = 6;
+      }else{
+        cTotal = trackSpec["트랙응용교과"].length;
+      }
+      for(let i=0; i< trackSpec["트랙기초교과"].length; i++){
+        for(let j in lectures){
+          if(lectures[j][7]==trackSpec["트랙기초교과"][i]){
+            pCount++;
+            if(pCount>1){
+              pText+=", ";
+            }
+            pText+=trackSpec["트랙기초교과"][i];
+          }
+        }
+      }
+      for(let i=0; i< trackSpec["트랙응용교과"].length; i++){
+        for(let j in lectures){
+          if(lectures[j][7]==trackSpec["트랙응용교과"][i]){
+            cCount++;
+            if(cCount>1){
+              cText+=", ";
+            }
+            cText+=trackSpec["트랙응용교과"][i];
+          }
+        }
+      }
+      let pPercent = pCount*100/(pTotal+cTotal);
+      let cPercent = cCount*100/(pTotal+cTotal);
+      return(
+        <div>
+          <p>{trackName}</p>
+          <ProgressBar>
+              <ProgressBar striped bsStyle="success" now={pPercent} key={1} label={pText} />
+              <ProgressBar bsStyle="warning" now={cPercent} key={2} label={cText} />
+            </ProgressBar>
+        </div>
+      );
+    }
+
+    renderTrack(lectures){
+      if(lectures == null){
+        return(<Panel
+        header="트랙 별 나의 이수 현황">
+            <Alert bsStyle="warning">로그인이 먼저 필요합니다.</Alert>
+        </Panel>);
+      }
+      let items = [];
+      for(let key in TrackInfo){
+        items.push(this.renderSingleTrack(key,TrackInfo[key],lectures));
+      }
+      return(
+        <Panel
+        header="트랙 별 나의 이수 현황">
+            {items}
+        </Panel>
+      );
+    }
+
     render() {
+      let lectures;
+      if(localStorage.getItem('user')!=null){
+        lectures = JSON.parse(localStorage.getItem('user')).lecture;
+      }else{
+        lectures = null;
+      }
+
         return (
             <div>
                 <Row>
@@ -19,14 +93,7 @@ class Mytrack extends React.Component {
                 </Row>
                 <Row>
                   <Col>
-                    <Panel
-                    header="트랙 별 나의 이수 현황">
-                        <ProgressBar>
-                            <ProgressBar striped bsStyle="success" now={35} key={1} label={35} />
-                            <ProgressBar bsStyle="warning" now={20} key={2} />
-                            <ProgressBar active bsStyle="danger" now={10} key={3} />
-                        </ProgressBar>
-                    </Panel>
+                    {this.renderTrack(lectures)}
                   </Col>
                 </Row>
                 <Row>
